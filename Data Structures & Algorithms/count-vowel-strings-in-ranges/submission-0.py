c276284ba73934c0b0763027e44d217e8eb0e1ab
Claudiu Mihai Jechel <44757034+ClaoMike@users.noreply.github.com>
2026-08-23 19:53:30 +0300

    Add: count-vowel-strings-in-ranges - submission-0

---
+++ Data Structures & Algorithms/count-vowel-strings-in-ranges/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
+        vowels = set()
+        vowels.add('a')
+        vowels.add('e')
+        vowels.add('i')
+        vowels.add('o')
+        vowels.add('u')
+
+        ans = []
+        prefix = [1] if words[0][0] in vowels and words[0][-1] in vowels else [0]
+
+        for i in range(1, len(words)):
+            if words[i][0] in vowels and words[i][-1] in vowels:
+                prefix.append(prefix[-1] + 1)
+            else:
+                prefix.append(prefix[-1])
+        
+        print(prefix)
+        for query in queries:
+            left, right = query
+            current = 1 if words[left][0] in vowels and words[left][-1] in vowels else 0
+            ans.append(prefix[right] - prefix[left] + current)
+
+        return ans
