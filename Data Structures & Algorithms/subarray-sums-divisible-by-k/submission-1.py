434e9a97bff81141d0e171fe7ee8214e641e2d44
Claudiu Mihai Jechel <44757034+ClaoMike@users.noreply.github.com>
2026-08-24 21:42:27 +0300

    Add: subarray-sums-divisible-by-k - submission-1

---
+++ Data Structures & Algorithms/subarray-sums-divisible-by-k/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        count = 0
+
+        prefix = [nums[0]]
+        for i in range(1, len(nums)):
+            prefix.append(prefix[-1] + nums[i])
+
+        from collections import defaultdict
+        hm = defaultdict(int)
+        hm[0] = 1
+
+        for i in range(len(prefix)):
+            remainder = prefix[i] % k
+            if remainder in hm:
+                count += hm[remainder]
+            
+            hm[remainder] += 1
+
+        return count
