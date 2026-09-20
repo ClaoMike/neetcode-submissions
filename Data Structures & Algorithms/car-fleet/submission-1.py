408c90e8795ca462e5c9a531844456ba72d7ee4d
Claudiu Mihai Jechel <44757034+ClaoMike@users.noreply.github.com>
2026-09-20 22:33:29 +0300

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        class Car:
+            def __init__(self, position, speed):
+                self.position = position
+                self.speed = speed
+                self.hours = (target - position) / speed
+
+        cars = []
+        for i, pos in enumerate(position):
+            cars.append( Car(position=pos, speed=speed[i]) )
+        
+        cars.sort(key=lambda x: x.position)
+
+        # for i in range(len(cars)):
+        #     print(f"Position: {cars[i].position} Speed: {cars[i].speed} Hours: {cars[i].hours}")
+
+        for i in reversed(range(len(cars)-1)):
+            if cars[i].hours < cars[i+1].hours:
+                cars[i].hours = cars[i+1].hours
+
+        # for i in range(len(cars)):
+        #     print(f"Position: {cars[i].position} Speed: {cars[i].speed} Hours: {cars[i].hours}")
+        
+        total = 0
+
+        while cars:
+            curr = cars.pop()
+            while cars and cars[-1].hours == curr.hours:
+                cars.pop()
+            total += 1
+
+        return total
